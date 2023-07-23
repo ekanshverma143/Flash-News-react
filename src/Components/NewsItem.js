02ac79d4641fa08be70d1a7eb37c5868a0f711a7
Ekansh Verma <21bee083@nith.ac.in>
2023-07-23 06:56:10 +0530

    newsitem.js updated

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
-export class NewsItem extends Component {
+export  class NewsItem extends Component {
   render() {
+      let {title, description ,imageUrl} = this.props
     return (
-      <div>This is News Item</div>
+      <div>
+          <div  className="card" style={{width: "18rem;"}}>
+            <img src={imageUrl}   classNameName="card-img-top" alt="..."/>
+            <div  className="card-body">
+                <h5  className="card-title"><title>{title}</title></h5>
+                <p  className="card-text">{description}</p>
+                <a href="/news.js"  className="btn btn-primary">Go somewhere</a>
+        </div>
+</div>
+
+      </div>
     )
   }
 }
