9c164f452d43d427b29e46ccca6beab3a7310f55
Ekansh Verma <21bee083@nith.ac.in>
2023-07-23 09:16:58 +0530

    api added, home page done

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export  class NewsItem extends Component {
   render() {
-      let {title, description ,imageUrl} = this.props
+      let {title, description ,imageUrl, newsUrl} = this.props
     return (
       <div>
-          <div  className="card" style={{width: "18rem;"}}>
-            <img src={imageUrl}   classNameName="card-img-top" alt="..."/>
+          <div  className="card" style={{width: "18rem"}}>
+            <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2023/07/22/600x338/PM-Modi-_1690006914431_1690006914681.jpg":imageUrl}   className="card-img-top" alt="..."/>
             <div  className="card-body">
-                <h5  className="card-title"><title>{title}</title></h5>
+                <h5  className="card-title">{title}</h5>
                 <p  className="card-text">{description}</p>
-                <a href="/news.js"  className="btn btn-primary">Go somewhere</a>
+                <a href= {newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary">Read More</a>
         </div>
 </div>
 
