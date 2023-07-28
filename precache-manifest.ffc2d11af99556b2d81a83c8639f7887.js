d4997e73ecdf6b890e6331635b4b18d95af4e9bc
Ekansh Verma <21bee083@nith.ac.in>
2023-07-28 17:43:24 +0530

    Updates

---
+++ precache-manifest.ffc2d11af99556b2d81a83c8639f7887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e3fa79b0957104afb08",
+    "url": "/Flash-News-react/static/css/main.4c75918e.chunk.css"
+  },
+  {
+    "revision": "0e3fa79b0957104afb08",
+    "url": "/Flash-News-react/static/js/main.0e3fa79b.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/Flash-News-react/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "85075142ed0c137ac113",
+    "url": "/Flash-News-react/static/js/2.85075142.chunk.js"
+  },
+  {
+    "revision": "e57fb2f25259ff847cfb",
+    "url": "/Flash-News-react/static/js/runtime~main.e57fb2f2.js"
+  },
+  {
+    "revision": "1dd61d6bd22d8f705452f28313477264",
+    "url": "/Flash-News-react/static/media/Loading.1dd61d6b.gif"
+  },
+  {
+    "revision": "0b86369f3f5de9bb3fd1e4348477fc71",
+    "url": "/Flash-News-react/index.html"
+  }
+];
